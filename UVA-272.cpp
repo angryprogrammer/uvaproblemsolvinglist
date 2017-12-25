04468329fc60f013d87010e89630a4e3527743ea
angryprogrammer <arabimoin@rocketmail.com>
2017-12-25 12:31:05 +0600

    UVA-272

---
+++ UVA-272.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+char ch;
+long int count=0;
+while(scanf("%c",&ch)==1)
+{
+    if(ch=='"')
+    {
+        count++;
+        if(count%2==1)
+        printf("``");
+        else if(count%2==0)
+        printf("''");
+    }
+    else printf("%c",ch);
+}
+}
